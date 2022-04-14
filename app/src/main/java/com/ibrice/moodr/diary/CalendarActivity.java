237846e54fdc2986cf8fa0201f4842e3f0dac46f
Isaac Basque-Rice <ibasquerice@gmail.com>
2022-04-14 15:46:06 +0100

    Added time and date to CalendarActivity and allowed switching between that and InputActivity

--- app/src/main/java/com/ibrice/moodr/diary/CalendarActivity.java
+++ app/src/main/java/com/ibrice/moodr/diary/CalendarActivity.java
@@ -2,15 +2,46 @@ package com.ibrice.moodr.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ibrice.moodr.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class CalendarActivity extends AppCompatActivity {
+
+    TextView txtDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        txtDate = findViewById(R.id.txtDate);
+        setDate(txtDate);
+
+        FloatingActionButton fab = findViewById(R.id.fabAdd);
+        fab.setOnClickListener(view ->  {
+            Intent fabIntent = new Intent(this, InputActivity.class);
+            fabIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(fabIntent);
+        });
+
+    }
+
+    // set today's date
+    @SuppressLint("SetTextI18n")
+    public void setDate(TextView view) {
+        Date today = Calendar.getInstance().getTime();
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat format
+                = new SimpleDateFormat("EEE, d MMM yyyy");
+        String date = format.format(today);
+        view.setText("Today's date is" + " " + date);
     }
 }
