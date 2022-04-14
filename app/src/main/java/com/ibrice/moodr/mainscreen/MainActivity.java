19d9c7771b91c4c594fdb6605f1b5f4a3d8e5dd2
Isaac Basque-Rice <ibasquerice@gmail.com>
2022-04-14 15:36:41 +0100

    Made buttons respond to onclic events and launch new activity

--- app/src/main/java/com/ibrice/moodr/mainscreen/MainActivity.java
+++ app/src/main/java/com/ibrice/moodr/mainscreen/MainActivity.java
@@ -2,9 +2,16 @@ package com.ibrice.moodr.mainscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ibrice.moodr.R;
+import com.ibrice.moodr.diary.CalendarActivity;
+import com.ibrice.moodr.habits.HabitsActivity;
+import com.ibrice.moodr.reports.ReportsActivity;
+import com.ibrice.moodr.settings.SettingsActivity;
+import com.ibrice.moodr.threegoodthings.ThreeGoodThingsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // functions that respond to onclick events for relevant buttons
+    public void openDiary(View view) {
+        Intent diaryIntent = new Intent(this, CalendarActivity.class);
+        diaryIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); // will not run if already at the top
+        startActivity(diaryIntent);
+    }
+
+    public void openHabits(View view) {
+        Intent habitsIntent = new Intent(this, HabitsActivity.class);
+        habitsIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(habitsIntent);
+    }
+
+    public void openThreeGoodThings(View view) {
+        Intent threegoodthingsIntent = new Intent(this, ThreeGoodThingsActivity.class);
+        threegoodthingsIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(threegoodthingsIntent);
+    }
+
+    public void openReports(View view) {
+        Intent reportsIntent = new Intent(this, ReportsActivity.class);
+        reportsIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(reportsIntent);
+    }
+
+    public void openSettings(View view) {
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        settingsIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(settingsIntent);
     }
 }
